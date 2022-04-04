dac9cfb2942343741026300a817fd7f12ed9bfbe
OscarNavi <101269440+OscarNavi@users.noreply.github.com>
2022-04-03 23:48:15 -0500

    curso

---
+++ 12_PRACTICAS_NAVI/js/practica3.2J-30-03.js
@@ -0,0 +1,16 @@
+
+            let marca,modelo,descuento = 0;
+            marca= prompt("Marca?","0");
+            modelo= prompt("Modelo?","0");
+            if((marca=="ford")&&(modelo=="fiesta"))
+        {
+        documento=5;
+
+        }
+        if((marca=="ford")&&(modelo=="fiesta"))
+        {
+            descuento=10;
+        }
+        document.write("<h3>Su descuento es ${descuento}</h3>");
+        
+        
